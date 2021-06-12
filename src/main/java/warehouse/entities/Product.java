3d6fc783a8052af5f5d46ce628dc3b8b7bbfe6b3
Janis Dreimanis <jan.dreimanis@gmail.com>
2021-06-12 22:57:44 +0300

    Changes by dator

--- src/main/java/warehouse/entities/Product.java
+++ src/main/java/warehouse/entities/Product.java
@@ -1,4 +1,77 @@
 package warehouse.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "products")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private Integer quantity;
+    private String unit;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Integer quantity, String unit, Double price) {
+        this.name = name;
+        this.quantity = quantity;
+        this.unit = unit;
+        this.price = price;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", quantity=" + quantity +
+                ", unit='" + unit + '\'' +
+                ", price=" + price +
+                '}';
+    }
 }
