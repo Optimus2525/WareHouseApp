20838a9c63bb9d46827b3937d35400f7c4e4b999
Ilmars <ilmarsm@gmail.com>
2021-06-14 15:46:32 +0300

    Added some additional stage options (commented)

--- src/main/java/warehouse/WarehouseApplication.java
+++ src/main/java/warehouse/WarehouseApplication.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import org.hibernate.type.TrueFalseType;
@@ -18,11 +19,19 @@ public class WarehouseApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         Parent rootScene = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/userInterface/main.fxml")));
-        stage.setTitle("Warehouse");
+        stage.setTitle("The Best Warehouse of The World ");
         stage.setScene(new Scene(rootScene));
+
+//        Additional options for primary Stage
+//        stage.setX(50);
+//        stage.setY(50);
+//        stage.setWidth(600);
+//        stage.setHeight(400);
+//        stage.initStyle(StageStyle.DECORATED);
+//        stage.setFullScreen(true);
+
         stage.show();
     }
-
 
 
     public static void main(String[] args) {
